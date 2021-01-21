8a37fb74b0d1f5605985cdb9134bb7be7281978e
Eugene Zaporozhets <euza@ciklum.com>
2021-01-21 22:52:44 +0200

    testcommit and add change rights for user roles

--- symfony/src/Controller/DefaultController.php
+++ symfony/src/Controller/DefaultController.php
@@ -9,19 +9,20 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-/**
-* @Route(
-*     "/contact",
-*     name="contact",
-*     condition="context.getMethod() in ['GET', 'HEAD'] and request.headers.get('User-Agent') matches '/firefox/i'"
-* )
-*
-* expressions can also include config parameters:
-* condition: "request.headers.get('User-Agent') matches '%app.allowed_browsers%'"
-*/
-public function contact(): Response
-{
-// ...
+    /**
+     * @Route(
+     *     "/contact",
+     *     name="contact",
+     *     condition="context.getMethod() in ['GET', 'HEAD'] and request.headers.get('User-Agent') matches '/firefox/i'"
+     * )
+     *
+     * expressions can also include config parameters:
+     * condition: "request.headers.get('User-Agent') matches '%app.allowed_browsers%'"
+     */
+    public function index(): Response
+    {
+        return new Response('Hello world');
+    }
 }
-}
+
 ?>
