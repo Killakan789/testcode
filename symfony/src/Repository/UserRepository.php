19b3de62b8088aca3e3adf0c4c9263f27600484f
Eugene Zaporozhets <euza@ciklum.com>
2021-03-04 10:50:40 +0200

    test with user credentials

--- symfony/src/Repository/UserRepository.php
+++ symfony/src/Repository/UserRepository.php
@@ -36,24 +36,6 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $this->_em->flush();
     }
 
-    // /**
-    //  * @return User[] Returns an array of User objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('u.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
     public function findOneBySomeField($value): ?User
     {
         return $this->createQueryBuilder('u')
@@ -63,5 +45,5 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
             ->getOneOrNullResult()
         ;
     }
-    */
+
 }
